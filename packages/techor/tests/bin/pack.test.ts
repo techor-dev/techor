8801a43fdc1a44875669948cac8838192c520a2e
1Aron <aron@master.co>
2023-03-31 20:26:46 +0800

    Fix tests

--- packages/techor/tests/bin/pack.test.ts
+++
@@ -1,7 +0,0 @@
-import { execSync } from 'node:child_process'
-import { expectExist } from '../../../../utils/expect-exist'
-
-it('build `bin` by `package.json`', () => {
-    execSync('tsx ../../src/bin pack', { cwd: __dirname, stdio: 'pipe' })
-    expectExist(['dist/bin/index.js'])
-})
