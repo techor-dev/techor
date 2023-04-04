4c23e95ace344b42ff0d65542da2a62b3d94f114
1aron <i@aron.tw>
2023-04-04 21:08:34 +0800

    Improve(Jest Puppeteer): Using `jest-puppeteer`

--- packages/jest-puppeteer/jest-preset.js
+++ packages/jest-puppeteer/jest-preset.js
@@ -1,6 +1,4 @@
 module.exports = {
     ...require('../jest/jest-preset'),
-    'globalSetup': 'jest-environment-puppeteer/setup',
-    'globalTeardown': 'jest-environment-puppeteer/teardown',
-    'testEnvironment': 'jest-environment-puppeteer'
+    'preset': 'jest-puppeteer'
 }
