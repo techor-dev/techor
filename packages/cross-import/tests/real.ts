9f45a95495b0571860450858d84e5fcefe4229a6
1aron <i@aron.tw>
2023-04-12 21:15:15 +0800

    Update tests

--- packages/cross-import/tests/real.ts
+++ packages/cross-import/tests/real.ts
@@ -2,6 +2,9 @@ import crossImport from '../src'
 import fs from 'fs'
 import path from 'path'
 
+/**
+ * 測試 require cache 在真實 Node 環境運作時是否有被正確的清除
+ */
 const configPath = path.join(__dirname, 'config-tmp.js')
 
 fs.writeFileSync(configPath, 'module.exports = { a: 0 }')
