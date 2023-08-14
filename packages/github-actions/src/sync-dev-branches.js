21056bca8e5dd49d8080a650dc45e0a6e236bc85
1aron <i@aron.tw>
2023-08-14 12:22:25 +0800

    Fix(GitHub Actions): Rebase fails and falls back to merge

--- packages/github-actions/src/sync-dev-branches.js
+++ packages/github-actions/src/sync-dev-branches.js
@@ -1,16 +1,23 @@
 const core = require('@actions/core')
 const checkBranchExists = require('./check-branch-exist')
 const rebase = require('./rebase')
+const merge = require('./merge')
 
 module.exports = async function syncDevBranches() {
-    try {
-        const currentBranchName = process.env.GITHUB_REF_NAME
-        const targetBranchName = `dev/${currentBranchName}`
-        if (await checkBranchExists(targetBranchName)) {
+    const currentBranchName = process.env.GITHUB_REF_NAME
+    const targetBranchName = `dev/${currentBranchName}`
+    if (await checkBranchExists(targetBranchName)) {
+        try {
             await rebase(targetBranchName)
+        } catch (error) {
+            console.error('Rebase failed, trying merge')
+            console.error(error)
+            try {
+                await merge(targetBranchName)
+            } catch (error) {
+                core.setOutput('error', error.message)
+                core.setFailed(error.message)
+            }
         }
-    } catch (error) {
-        core.setOutput('error', error.message)
-        core.setFailed(error.message)
     }
 }
