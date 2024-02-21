d83d6ca734489bb943c9209856bb5df07c36529a
1aron <i@aron.tw>
2024-02-21 17:47:35 +0800

    Add build and import statements for client and server components

--- packages/techor/tests/react/test.ts
+++ packages/techor/tests/react/test.ts
@@ -4,9 +4,21 @@ import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 
 beforeAll(() => {
-    execSync('tsx ../../src/bin build', { cwd: __dirname, stdio: 'pipe' })
+    execSync('tsx ../../src/bin build "src/**/*.tsx"', { cwd: __dirname, stdio: 'pipe' })
 })
 
 test('App', () => {
     expect(readFileSync(join(__dirname, './dist/App.mjs'), 'utf-8')).toContain('React.createElement("h1", null, "Hello, World!")')
+    expect(readFileSync(join(__dirname, './dist/App.mjs'), 'utf-8')).toContain(`import Client from './components/Client.mjs';`)
+    expect(readFileSync(join(__dirname, './dist/App.mjs'), 'utf-8')).toContain(`import Server from './components/Server.mjs';`)
+})
+
+test('use client', () => {
+    expect(readFileSync(join(__dirname, './dist/components/Client.cjs'), 'utf-8')).toContain('"use client"')
+    expect(readFileSync(join(__dirname, './dist/components/Client.mjs'), 'utf-8')).toContain('"use client"')
+})
+
+test('use server', () => {
+    expect(readFileSync(join(__dirname, './dist/components/Server.cjs'), 'utf-8')).toContain('"use server"')
+    expect(readFileSync(join(__dirname, './dist/components/Server.mjs'), 'utf-8')).toContain('"use server"')
 })
