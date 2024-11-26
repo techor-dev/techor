caf372985e269d34c17419104d524616d0a91b45
Aron <i@aron.tw>
2024-11-27 04:13:38 +0800

    Update type-check command to install test dependencies before checking types

--- packages/techor/tests/external/src/index.ts
+++ packages/techor/tests/external/src/index.ts
@@ -1,10 +1,10 @@
 import extend from '@techor/extend' // deps
 import log from '@techor/log' // peerDeps
-import { del } from '@techor/log/dist' // wide module path
+import distLog from '@techor/log/dist' // wide module path
 
 // @ts-ignore
 import 'fake-external-package' // --external fake-external-package
 
 export * from './foo'
 
-console.log(extend, log, del)
+console.log(extend, log, distLog)
